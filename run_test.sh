e16fd859d32671b3fef2b980664d17ed44956a0a
Flo Windels <florent.windels@epitech.eu>
2025-02-12 18:57:41 +0100

    feat(run_test.sh): take all output

--- run_test.sh
+++ run_test.sh
@@ -4,20 +4,20 @@ echo "🚀 Lancement des tests..."
 
 while read -r cmd; do
     echo " "
-    echo "$cmd" | ../../mysh > output.txt
+    echo "$cmd" | ../../mysh &> output.txt
     if [ $? -ne 0 ]; then
         echo "❌ Votre shell a planté en exécutant '$cmd'"
         continue
     fi
-    echo "$cmd" | tcsh > expected_output.txt
-    diff_output=$(diff output.txt expected_output.txt)
+    echo "$cmd" | tcsh &> expected_output.txt
+    diff output.txt expected_output.txt
     if [ $? -eq 0 ]; then
         echo "✅ Test '$cmd' réussi"
     else
         echo " "
         echo "❌ Test '$cmd' échoué"
         echo "📌 Différences :"
-        echo "$diff_output"
+        diff output.txt expected_output.txt
     fi
 done < test_case.txt
 echo "🛠️ Tests terminés !"
