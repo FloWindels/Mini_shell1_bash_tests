1b78b70518200d3bda04052c3fab597a7b8916af
Flo Windels <florent.windels@epitech.eu>
2025-02-12 16:41:26 +0100

    ci(run_test.sh): Adding space for better readability and a new test: '/bin/ls'

--- run_test.sh
+++ run_test.sh
@@ -3,12 +3,14 @@
 echo "🚀 Lancement des tests..."
 
 while read -r cmd; do
+    echo " "
     echo "$cmd" | ../../mysh > output.txt
     echo "$cmd" | tcsh > expected_output.txt
     diff output.txt expected_output.txt
     if [ $? -eq 0 ]; then
         echo "✅ Test '$cmd' réussi"
     else
+        echo " "
         echo "❌ Test '$cmd' échoué"
         echo "📌 Différences :"
         diff output.txt expected_output.txt
